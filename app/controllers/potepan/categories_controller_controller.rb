452523a8fa3d008d5ec605dbf01af63559e49085
keikeito <n.keikeito0122@gmail.com>
2018-11-07 00:26:13 +0900

    Create a category page

--- app/controllers/potepan/categories_controller_controller.rb
+++ app/controllers/potepan/categories_controller_controller.rb
@@ -1,5 +1,7 @@
-class Potepan::CategoriesControllerController < ApplicationController
-  @taxon = Spree::Taxon.find(params[:id])
-  @products = Spree::Product.in_taxon(@taxon)
-  @taxonomies = Spree::Taxonomy.all
+class Potepan::CategoriesController < ApplicationController
+  def show
+    @taxon = Spree::Taxon.find(params[:id])
+    @products = Spree::Product.in_taxon(@taxon)
+    @taxonomies = Spree::Taxonomy.all
+  end
 end
