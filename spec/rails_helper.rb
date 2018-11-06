c905dc05290f17c4af586aecb410442f9b21ff9b
keikeito <n.keikeito0122@gmail.com>
2018-11-07 04:52:47 +0900

    Setting to output logs with RSpec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -27,6 +27,7 @@ require 'capybara/poltergeist'
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
+Rails.logger = Logger.new(STDOUT)
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
